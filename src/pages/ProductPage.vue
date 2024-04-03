17117455b3bb57ab5f36aa832fe1b54099a721ab
Diego Espinosa <78836469+yoGhastly@users.noreply.github.com>
2024-04-02 22:10:11 -0600

    Update ProductPage.vue

--- src/pages/ProductPage.vue
+++ src/pages/ProductPage.vue
@@ -2,7 +2,7 @@
   <header class="flex justify-start items-start px-6">
     <RouterLink to="/" class="text-gray-500 underline self-start">Go Back</RouterLink>
   </header>
-  <section class="flex h-screen">
+  <section class="flex flex-col md:flex-row h-screen">
     <picture class="relative block rounded-none w-1/2 h-full aspect-square group">
       <img :src="product.imageUrl" :alt="product.name"
         class="object-contain w-full rounded-none h-full pointer-events-none group-hover:scale-100 transition delay-0" />
